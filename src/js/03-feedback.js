9f968fb3c991cc76c2fcc442c3c4577269acff68
Alexeencokirill <97600028+Alexeencokirill@users.noreply.github.com>
2023-05-14 13:10:05 +0200

    hw

---
+++ src/js/03-feedback.js
@@ -0,0 +1,39 @@
+import { throttle } from "lodash";
+
+const form = document.querySelector('.feedback-form')
+const mail = document.querySelector('mail')
+const message = document.querySelector('message')
+
+
+form.addEventListener('input', throttle(evt => {
+    const input = {
+        email: form.elements.email.value,
+        message: form.elements.message.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(input));
+}, 500)
+);
+
+
+form.addEventListener('submit', evt => {
+    event.preventDefault();
+    const {
+        elements: {email, message},
+    } = event.currentTarget;
+    console.log({email: email.value, message: message.value});
+
+    evt.currentTarget.reset();
+    localStorage.clear();
+});
+
+const storage = localStorage.getItem('feedback-form-state')
+const parseStorageData = JSON.parse(storage);
+const tryFoo = () => {
+    if (parseStorageData !== null) {
+        email.value = parseStorageData.email;
+        text.value = parseStorageData.message
+    }
+}
+
+
+tryFoo();
